b79fb223aa788528b9f3366f9af2689e5356ef65
Enyer De Jesus Holguin Toribio <enyer_holguin@ucne.edu.do>
2020-03-24 21:19:18 -0400

    Creando entidades De Categoria,Articulo y cobros tambien cree el contexto

---
+++ Entidades/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SistemaVentas.Entidades
+{
+    public class Categoria
+    {
+        [Key]
+        public int CategoriaId { get; set; }
+        public string NombreCategoria { get; set; }
+
+        public Categoria()
+        {
+            CategoriaId = 0;
+            NombreCategoria = string.Empty;
+        }
+    }
+}
